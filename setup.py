90776543067ef19192f53b8e1cca12499a70fab7
the-real-finnventor <71413483+the-real-finnventor@users.noreply.github.com>
2023-11-29 08:15:29 -0300

    Change v number

--- setup.py
+++ setup.py
@@ -4,22 +4,18 @@ with open("ALLINONE.md", "r") as f:
 setup(
     name="finnsflask",
     packages=['finnsflask'],
-    version='1.1',
+    version='1.2',
     license="MIT",
     description='This is a recreation of the popular python package, Flask.',
-    author='Real Finnventor',                   # Type in your name
-    author_email='finnventor@everspaugh.com',      # Type in your E-Mail
-    # Provide either the link to your github or to your website
+    author='Real Finnventor',
+    author_email='finnventor@everspaugh.com',
     url='https://github.com/the-real-finnventor/finnsflask',
-    # I explain this later on
     download_url='https://github.com/the-real-finnventor/finnsflask/archive/refs/tags/1.1.tar.gz',
-    # Keywords that define your package best
     keywords=['FLASK', 'WEBSERVER', "JINJA2"],
-    install_requires=[            # I get to this in a second
+    install_requires=[
         'Jinja2'
     ],
     classifiers=[
-        # Chose either "3 - Alpha", "4 - Beta" or "5 - Production/Stable" as the current state of your package
         'Development Status :: 4 - Beta',
         'Intended Audience :: Developers',
         'Topic :: Software Development :: Build Tools',
